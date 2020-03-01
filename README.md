619ddfc5941035550cc3a9eacf4551eb53c9e938
Devin McManus <devinmcmanus@DMC-Home-MBP.local>
2020-03-01 14:27:33 -0500

    added documentation

---
+++ README.md
@@ -0,0 +1,12 @@
+# xmpl-parser
+Untangle evictions data
+
+
+
+```
+pip install untangle
+```
+
+Create /data and /output folders for inputs and outpust respectively
+
+see shell_scripts.sh for methos to cut down the XML files to a testable size
---
+++ shell_scripts.sh
@@ -0,0 +1,6 @@
+head -n 100 "data/LandlordTenantExtract2018.xml" > "output/2018_first100.xml"
+
+#you'll need to either manually manually add in closing tags or copy them from teh tail of the file
+
+tail  "data/LandlordTenantExtract2018.xml" > "output/2018_tail.xml"
+
